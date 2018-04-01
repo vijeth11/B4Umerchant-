9e034befcbd3b22b6e9ad23972ca014e19de4652
vijethbelle <vijeth.belle@juspay.in>
2018-04-01 12:19:32 +0530

    added update option

--- app/src/main/java/com/example/stpl/b4umertest/Main2Activity.java
+++ app/src/main/java/com/example/stpl/b4umertest/Main2Activity.java
@@ -23,6 +23,14 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 public class Main2Activity extends AppCompatActivity {
     private String TAG = MainActivity.class.getSimpleName();
 
@@ -50,8 +58,15 @@ public class Main2Activity extends AppCompatActivity {
         update.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                for(int i=1;i<selecteditems.length;i++)
+                {
+                    String urls ="http://vijeth11.000webhostapp.com/merchant.php?name="+name[i].replace(" ","+")+"&available=";
+                    if(selecteditems[i])
+                        updateTheData(urls+"no");
+                    else
+                        updateTheData(urls+"yes");
+                }
 
-                 new Upload(Main2Activity.this,url).execute();
             }
         });
 
@@ -59,10 +74,7 @@ public class Main2Activity extends AppCompatActivity {
     public void called(int pos,Boolean val)
     {
         selecteditems[pos+1]=val;
-        Toast.makeText(Main2Activity.this,name[pos+1]+"is checked"+String.valueOf(selecteditems[pos+1]),Toast.LENGTH_SHORT).show();
-
-
-
+        //Toast.makeText(Main2Activity.this,name[pos+1]+"is checked"+String.valueOf(selecteditems[pos+1]),Toast.LENGTH_SHORT).show();
     }
     /**
      * Async task class to get json by making HTTP call
@@ -167,5 +179,57 @@ public class Main2Activity extends AppCompatActivity {
          lv.setAdapter(new ImageAdapter(Main2Activity.this, Arrays.copyOfRange(name, 1, name.length)));
         }
 
+    }
+
+
+
+    private void updateTheData(String JSON_URL) {
+        //getting the progressbar
+
+
+        //creating a string request to send request to the url
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, JSON_URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        //hiding the progressbar after completion
+
+
+
+                        try {
+                            //getting the whole json object from the response
+                            JSONObject obj = new JSONObject(response);
+
+                            //we have the array named hero inside the object
+                            //so here we are getting that json array
+                           String error =obj.getString("error");
+                           String message=obj.getString("message");
+                           if(error=="TRUE")
+                           Toast.makeText(Main2Activity.this,message,Toast.LENGTH_SHORT).show();
+
+                            //now looping through all the elements of the json array
+
+
+                            //creating custom adapter object
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //displaying the error in toast if occurrs
+                        Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        //creating a request queue
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        //adding the string request to request queue
+        requestQueue.add(stringRequest);
     }
 }
